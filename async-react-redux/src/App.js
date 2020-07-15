886ad968034aa1a4cdadad767979fe27022309e3
Nic Lehman <niclehman@Nics-MacBook-Pro.local>
2020-07-15 15:59:43 -0500

    🎁 INIT: CRA + basic reset + redux related dependencies

---
+++ async-react-redux/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className='App'>
+      <div className='container'>
+        <h1>hello world</h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
